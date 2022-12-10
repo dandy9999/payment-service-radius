4fb37fa04401ae0077035f79f2ae9421a1b097b6
dandy9999 <dandyjos99@gmail.com>
2022-12-10 09:00:59 +0700

    update

--- app-Midtrans/app/config/config.php
+++ app-Midtrans/app/config/config.php
@@ -5,5 +5,5 @@ define('BASEURL', 'http://' . $_SERVER['SERVER_NAME'].':'.$_SERVER['SERVER_PORT'
 // DB
 define('DB_HOST', 'localhost');
 define('DB_USER', 'radius');
-define('DB_PASS'3q, 'pass');
+define('DB_PASS', 'pass');
 define('DB_NAME', 'radius');
