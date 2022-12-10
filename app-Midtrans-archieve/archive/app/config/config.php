08016eef1493b7812816343adad1efafb92850b2
dandy9999 <fericodeva69@gmail.com>
2022-12-10 06:26:20 +0000

    update change mvc burik to laravel

---
+++ app-Midtrans-archieve/archive/app/config/config.php
@@ -0,0 +1,10 @@
+<?php
+
+//define('BASEURL', 'http://' . $_SERVER['SERVER_NAME'].':'.$_SERVER['SERVER_PORT'] .'/public');
+define('BASEURL', 'http://4.193.44.15:8088/phpmvc/public');
+
+// DB
+define('DB_HOST', 'localhost');
+define('DB_USER', 'radius');
+define('DB_PASS', 'pass');
+define('DB_NAME', 'radius');
--- app-Midtrans/app/config/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-define('BASEURL', 'http://' . $_SERVER['SERVER_NAME'].':'.$_SERVER['SERVER_PORT'] .'/public');
-
-// DB
-define('DB_HOST', 'localhost');
-define('DB_USER', 'radius');
-define('DB_PASS', 'pass');
-define('DB_NAME', 'radius');
